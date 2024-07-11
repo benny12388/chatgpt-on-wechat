96810fa218439fb1c31598e178672d978840bd20
benny12388 <460421034@qq.com>
2024-07-11 19:10:11 +0800

    Update build.latest.sh

--- docker/build.latest.sh
+++ docker/build.latest.sh
@@ -3,6 +3,6 @@
 unset KUBECONFIG
 
 cd .. && docker build -f docker/Dockerfile.latest \
-             -t zhayujie/chatgpt-on-wechat .
+             -t benny12388/chatgpt-on-wechat .
 
-docker tag zhayujie/chatgpt-on-wechat zhayujie/chatgpt-on-wechat:$(date +%y%m%d)
+docker tag benny12388/chatgpt-on-wechat benny12388/chatgpt-on-wechat:$(date +%y%m%d)
